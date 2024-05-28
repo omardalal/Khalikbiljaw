cc3b3552c80a3e8d2a42068a0b5b37e4f53e5db4
Omar Dalal <omar.dalal@outlook.com>
2024-05-28 23:51:39 +0300

    Update FeaturedTourList.jsx

--- src/components/Featured-tours/FeaturedTourList.jsx
+++ src/components/Featured-tours/FeaturedTourList.jsx
@@ -4,7 +4,6 @@ import TourCard from "../../shared/TourCard";
 import { Col } from "reactstrap";
 
 import useFetch from "../../hooks/useFetch";
-import { BASE_URL } from "../../utils/config";
 
 const FeaturedTourList = () => {
   const {
